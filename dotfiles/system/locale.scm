6a2decd549a8225efde0fe9a974a2d933668600e
LFour <63118374+LFour-CN@users.noreply.github.com>
2025-04-21 12:26:46 +0800

    Delete dotfiles directory

--- dotfiles/system/locale.scm
+++
@@ -1,8 +0,0 @@
-(define-module (dotfiles system locale)
-  #:export (locale-configuration))
-
-(define locale-configuration
-  (list
-   (locale "en_US.utf8")
-   (timezone "Asia/Shanghai")
-   (keyboard-layout (keyboard-layout "us"))))
